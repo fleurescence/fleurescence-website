fa2706dba878c5c11129fe8f7b4843a4d375fe81
fleurescence <ingtest354@yahoo.com>
2022-04-28 16:08:57 +0300

    finished basis for packs and video

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    
+    $('#pack-play-1').click(() => {
+        let audio = new Audio('/fleurescence-website/assets/images/packs/pack-source/random-home-stuff-preview.wav');
+        audio.play();
+    });
+
+    $('#pack-play-2').click(() => {
+        let audio = new Audio('/fleurescence-website/assets/images/packs/pack-source/nature-vol1-preview.wav');
+        audio.play();
+    });
+
+    $('#pack-play-3').click(() => {
+        let audio = new Audio('/fleurescence-website/assets/images/packs/pack-source/glitchy-space-stuff-preview.wav');
+        audio.play();
+    });
+});
