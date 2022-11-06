abdaaaa6141088f23794819d4e41c6f13d200ef3
fleurescence <fleurescencemusic@gmail.com>
2022-11-06 18:05:56 +0200

    fixed file routing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,43 +2,43 @@ $(document).ready(function() {
     
     let pack1 =  $('#pack-play-1');
     let audio1 = document.createElement("audio");
-    audio1.src = '/fleurescence-website/assets/images/packs/pack-source/random-home-stuff-preview.wav';
-    audio1.onended = () => pack1.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/play-button.svg\")"});
+    audio1.src = '/assets/images/packs/pack-source/random-home-stuff-preview.wav';
+    audio1.onended = () => pack1.css({"background-image" : "url(\"/assets/images/packs/play-button.svg\")"});
     pack1.click(() => {
         if(audio1.paused){
             audio1.play();
-            pack1.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/pause-button.svg\")"});
+            pack1.css({"background-image" : "url(\"/assets/images/packs/pause-button.svg\")"});
         } else {
             audio1.pause();
-            pack1.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/play-button.svg\")"});
+            pack1.css({"background-image" : "url(\"/assets/images/packs/play-button.svg\")"});
         }
     });
 
     let pack2 =  $('#pack-play-2');
     let audio2 = document.createElement("audio");
-    audio2.src = '/fleurescence-website/assets/images/packs/pack-source/nature-vol1-preview.wav';
-    audio2.onended = () => pack2.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/play-button.svg\")"});
+    audio2.src = '/assets/images/packs/pack-source/nature-vol1-preview.wav';
+    audio2.onended = () => pack2.css({"background-image" : "url(\"/assets/images/packs/play-button.svg\")"});
     pack2.click(() => {
         if(audio2.paused){
             audio2.play();
-            pack2.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/pause-button.svg\")"});
+            pack2.css({"background-image" : "url(\"/assets/images/packs/pause-button.svg\")"});
         } else {
             audio2.pause();
-            pack2.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/play-button.svg\")"});
+            pack2.css({"background-image" : "url(\"/assets/images/packs/play-button.svg\")"});
         }
     });
 
     let pack3 =  $('#pack-play-3');
     let audio3 = document.createElement("audio");
-    audio3.src = '/fleurescence-website/assets/images/packs/pack-source/glitchy-space-stuff-preview.wav';
-    audio3.onended = () => pack3.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/play-button.svg\")"});
+    audio3.src = '/assets/images/packs/pack-source/glitchy-space-stuff-preview.wav';
+    audio3.onended = () => pack3.css({"background-image" : "url(\"/assets/images/packs/play-button.svg\")"});
     pack3.click(() => {
         if(audio3.paused){
             audio3.play();
-            pack3.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/pause-button.svg\")"});
+            pack3.css({"background-image" : "url(\"/assets/images/packs/pause-button.svg\")"});
         } else {
             audio3.pause();
-            pack3.css({"background-image" : "url(\"/fleurescence-website/assets/images/packs/play-button.svg\")"});
+            pack3.css({"background-image" : "url(\"/assets/images/packs/play-button.svg\")"});
         }
     });
 
--- index.markdown
+++ index.markdown
@@ -4,13 +4,13 @@ layout: empty
 
 <div class="landing-container">
 <div class="general-landing">
-<img class="psst" src="/fleurescence-website/assets/images/landing/psst-icon.svg"/>
+<img class="psst" src="/assets/images/landing/psst-icon.svg"/>
 <div class="d-flex">
-<img class="click-here" src="/fleurescence-website/assets/images/landing/click-arrow.svg"/>
+<img class="click-here" src="/assets/images/landing/click-arrow.svg"/>
 
-<a class="circle" href='/fleurescence-website/packs'><img class="img-fluid" src="/fleurescence-website/assets/images/landing/circle.svg"/></a>
+<a class="circle" href='/packs'><img class="img-fluid" src="/assets/images/landing/circle.svg"/></a>
 
 </div>
-<img class="guy" src="/fleurescence-website/assets/images/landing/rufus-pattern.png"/>
+<img class="guy" src="/assets/images/landing/rufus-pattern.png"/>
 </div>
 </div>
